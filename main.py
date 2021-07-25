94886280664541edfe1878a813a4869a502e94e9
John Hadden <59323260+jhadden04@users.noreply.github.com>
2021-07-25 12:04:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,11 @@ from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 import os
 import math
 import time
-
-os.environ["SPOTIPY_CLIENT_ID"] = '04e9593b8574411ea1640433ea464fb8'
-os.environ["SPOTIPY_CLIENT_SECRET"] = 'dd80d98d0c9845a6bef36bc4d10c308a'
-os.environ["SPOTIPY_REDIRECT_URI"] = 'http://localhost:8080'  # all the auth
-username = "bytmjoltm7yu5hq50um4ik6sb"
-device_id = 'b0a12c11905ae21baa4400d765bbca53a6df2e0b'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
 import spotipy.util as util
 
-client_id = '04e9593b8574411ea1640433ea464fb8'
-client_secret = 'dd80d98d0c9845a6bef36bc4d10c308a'
+username = "*"
+client_id = '*'
+client_secret = '*'
 redirect_uri = 'http://localhost:8080'
 
 scope = "playlist-modify-public"
@@ -28,7 +21,6 @@ else:
     print("Can't get token for", username)
 playlists = (sp.current_user_playlists())
 
-# print(playlists["items"][0])
 playlist_names = []
 playlist_ids = []
 for i in range(len(playlists['items'])):
@@ -49,18 +41,14 @@ for i in range(len(playlist_info)):
         track_ids.append(playlist_info[i]["tracks"]["items"][j]["track"]["id"])
 
 playlist_tracks = list(dict.fromkeys(playlist_tracks))
-# print(playlist_tracks)
 
 track_ids = list(dict.fromkeys(track_ids))
-# print(track_ids)
-# playlist_add_items(playlist_id, items, position=None)
 title = "All Tracks"
 
 split_up_tracks = {}
 for i in range(math.ceil((len(track_ids) / 100))):
     try:
         split_up_tracks[i] = track_ids[i * 100:i * 100 + 100]
-        # print(i*100,i*100+99)
     except:
         split_up_tracks[i] = track_ids[i * 100:]
 
@@ -75,14 +63,11 @@ else:
     sp.user_playlist_create(username, title, public=True)
 
     playlists = sp.current_user_playlists()
-    # print(playlists)
     for i in range(len(playlists['items'])):
         playlist_names.append(playlists["items"][i]["name"])
         playlist_ids.append(playlists["items"][i]["id"])
-    # index1 = playlist_names.index(title)
 
     for i in range(math.ceil((len(track_ids) / 100))):
         sp.user_playlist_add_tracks(username, playlist_ids[0], split_up_tracks[i], position=None)
-        print(len(split_up_tracks[i])+1)
 
 
